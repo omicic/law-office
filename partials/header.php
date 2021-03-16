5943723ed922aa97a3d1cade15ada0ff03522a00
Olivera Micic <omicic@gmail.com>
2021-03-16 11:01:54 +0100

    header

---
+++ partials/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Advokat</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+    <section class="topbar container">
+        <a class="logo" href="index.php">Law <span>Office</span></a>
+        <ul>
+            <li><a href=""><i class="fab fa-facebook"></a></i></li>
+            <li><a href=""><i class="fab fa-twitter"></a></i></li>
+            <li><a href=""><i class="fab fa-instagram"></a></i></li>
+        </ul>
+    </section>
+
+    <header class="mainHeader">
+        <nav class="container">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+        <hgroup>
+            <h1>Experience - Trust - Results</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quos consequuntur veniam laborum veritatis
+                nulla neque beatae a dolorem praesentium dolor sit, amet consectetur adipisicing elit. Sint
+                quos consequuntur veniam laborum veritatis
+                nulla neque beatae a dolorem praesentium.</p>
+            <button>View more</button>
+        </hgroup>
+    </header>
